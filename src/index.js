57e28d43a2389a452022da8f13b6795e9a08eff9
tacticalParmesan <edoardoferreri10@gmail.com>
2024-04-25 19:01:46 +0200

    Implemented tab switching logic!

--- src/index.js
+++ src/index.js
@@ -3,17 +3,49 @@ import { Home } from "./modules/home.js";
 import { About } from "./modules/about.js";
 import "./style.css";
 
-const content = document.querySelector("#content");
-const body = document.querySelector("body");
+const PageRouter = function() {
+	const content = document.querySelector("#content");
+	const body = document.querySelector("body");
+	const pages = {
+		"Home": () => Home(content),
+		"About": () => About(content)
+	}
 
-document.addEventListener("DOMContentLoaded", () => {
-	About(content);
-	footer();
-});
 
-const footer = () =>
+	function loadLanding() {
+		document.addEventListener("DOMContentLoaded", () => {
+			loadTabSwitching();
+			Home(content);
+			footer();
+		});
+	}
+
+	function switchPage(pageName) {
+		wipePage();
+		pages[pageName]()
+	}
+
+	function wipePage() {
+		content.removeChild(content.firstChild);
+	}
+
+	function loadTabSwitching() {
+		const navButtons = document.querySelectorAll("nav > button");
+		navButtons.forEach((btn) => {
+			btn.addEventListener("mousedown", (clickEvent) =>{
+				switchPage(clickEvent.target.textContent)
+			})
+		})
+	}
+
+	const footer = () =>
 	createElement({
 		type: "footer",
 		text: "Restaurant page project by @tacticalParmesan | Odin Project 2024",
 		parent: body,
 	});
+
+	return {loadLanding, wipePage}
+}();
+
+PageRouter.loadLanding()
