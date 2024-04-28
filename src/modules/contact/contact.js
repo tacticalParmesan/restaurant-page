92ff81f2e2fdef3648506d6a0a4c176ceebb9b28
tacticalParmesan <edoardoferreri10@gmail.com>
2024-04-28 22:20:13 +0200

    Finished contact page and with it the whole project!

--- src/modules/contact/contact.js
+++ src/modules/contact/contact.js
@@ -2,6 +2,7 @@ import { createElement } from "../../utility.js";
 import "./contact.css";
 
 export function Contact(content) {
+
 	const contactPage = createElement({
 		classes: ["contact-page"],
 		parent: content,
@@ -95,6 +96,9 @@ export function Contact(content) {
 		id: "sender-mail",
 		classes: ["sender-mail-field", "form-input"],
 		parent: emailRow,
+        overload: (elm) => {
+            elm.required = true
+        }
 	});
 
 	const senderMessage = createElement({
@@ -106,13 +110,12 @@ export function Contact(content) {
 
 	const sendMessageButton = createElement({
 		type: "input",
-		inputType: "button",
+		inputType: "submit",
 		text: "Send message",
 		parent: sendMessageForm,
 		overload: (elm) => {
 			elm.value = "Send message";
-			elm.onclick = () => {
-				alert("Message sent! Thank you!");
+			elm.onsubmit = (ev) => {
 				[senderName, senderMail, senderMessage].forEach(
 					(field) => (field.value = "")
 				);
@@ -123,13 +126,121 @@ export function Contact(content) {
 	const bookingContainer = createElement({
 		classes: ["booking-container", "info-panel"],
 		parent: contactPage,
-        overload: (bookingCtn) => {
-            createElement({
-                type: "h2",
-                classes: ["booking-title"],
-                text: "Make a reservation",
-                parent: bookingCtn
-            })
+		overload: (bookingCtn) => {
+			createElement({
+				type: "h2",
+				classes: ["booking-title"],
+				text: "Make a reservation",
+				parent: bookingCtn,
+			});
+		},
+	});
+
+	const bookingForm = createElement({
+		type: "form",
+		classes: ["booking-form"],
+		parent: bookingContainer,
+	});
+
+	const dateRow = createElement({
+		classes: ["form-row", "date-row"],
+		parent: bookingForm,
+		overload: (row) => {
+			createElement({
+				type: "label",
+				forLabel: "date",
+				classes: ["date-label"],
+				text: "Date:",
+				parent: row,
+			});
+		},
+	});
+
+	const dateField = createElement({
+		type: "input",
+		inputType: "date",
+		id: "date",
+		parent: dateRow,
+	});
+
+	const peopleRow = createElement({
+		classes: ["form-row", "people-row"],
+		parent: bookingForm,
+		overload: (row) => {
+			createElement({
+				type: "label",
+				forLabel: "people",
+				classes: ["people-label"],
+				text: "Guests:",
+				parent: row,
+			});
+		},
+	});
+
+	const peopleField = createElement({
+		type: "input",
+		inputType: "number",
+		id: "people",
+		parent: peopleRow,
+        overload: (elm) => {
+            elm.setAttribute("min", 1)
         }
+	});
+
+	peopleField.addEventListener("input", () => {
+		if (peopleField.value <= 8) {
+			let guests = "👤".repeat(peopleField.value);
+			peopleIcons.textContent = guests;
+		} else {
+            peopleIcons.textContent = "Sorry! We don't have tables this big!"
+        }
+	});
+
+    const confirmationEmailRow = createElement({
+		classes: ["form-row", "confirmation-mail-row"],
+		parent: bookingForm,
+		overload: (row) => {
+			createElement({
+				type: "label",
+				classes: ["confirmation-mail-label", "form-input"],
+				forLabel: "Confirmation-mail",
+				text: "Your email:",
+				parent: row,
+                overload: (elm) => {
+                    elm.required = true
+                }
+			});
+		},
+	});
+
+	const confirmationMail = createElement({
+		type: "input",
+		inputType: "email",
+		id: "sender-mail",
+		classes: ["confirmation-mail-field", "form-input"],
+		parent: confirmationEmailRow,
+	});
+
+    const peopleIcons = createElement({
+		type: "span",
+		classes: ["people-icons"],
+		text: "👤",
+		parent: bookingForm,
+	});
+
+    const bookingButton = createElement({
+		type: "input",
+		inputType: "submit",
+		text: "Make a reservation",
+		parent: bookingForm,
+		overload: (elm) => {
+			elm.value = "Make a reservation";
+			elm.onsubmit = () => {
+				alert("Thanks for booking a table! We have sent a confirmation email!");
+				[dateField, peopleField, confirmationMail].forEach(
+					(field) => (field.value = "")
+				);
+			};
+		},
 	});
 }
